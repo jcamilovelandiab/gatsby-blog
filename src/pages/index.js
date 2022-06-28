3c7fcbd8a1b64dfdf957ce9d003e214ada47f994
Camilo <jcamilovelandiab@gmail.com>
2022-06-28 17:29:23 -0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 
-const links = [
+/*const links = [
   {
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial",
@@ -31,7 +32,7 @@ const links = [
     description:
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
   },
-]
+]*/
 
 const samplePageLinks = [
   {
@@ -46,7 +47,7 @@ const samplePageLinks = [
   { text: "Deferred Static Generation", url: "using-dsg" },
 ]
 
-const moreLinks = [
+/*const moreLinks = [
   { text: "Join us on Discord", url: "https://gatsby.dev/discord" },
   {
     text: "Documentation",
@@ -65,11 +66,20 @@ const moreLinks = [
     url: "https://www.gatsbyjs.com/contributing/",
   },
   { text: "Issues", url: "https://github.com/gatsbyjs/gatsby/issues" },
-]
+]*/
 
-const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+//const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
-const IndexPage = () => (
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+` 
+
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
     <div className={styles.textCenter}>
@@ -97,26 +107,46 @@ const IndexPage = () => (
         Edit <code>src/pages/index.js</code> to update this page.
       </p>
     </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
+    <div>
+      <h1>Camilo's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {
+        data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
+            <p>{node.excerpt}</p>
+          </div>
+        ))
+      }
+    </div>
   </Layout>
 )
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          fields {
+            slug
+          }
+          excerpt
+          html
+        }
+      }
+    }
+  }
+`
